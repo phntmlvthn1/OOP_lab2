2b2217d578016924049c8094aae98c5dcbd9bc81
user name <grasstv14@gmail.com>
2021-11-18 22:15:27 +0500

    класс-потомок, его метод

--- OOP_lab2/Source.cpp
+++ OOP_lab2/Source.cpp
@@ -4,7 +4,8 @@ using namespace std;
 class Furniture
 {
 
-private:
+protected:
+
 	int length, width;
 
 public:
@@ -38,7 +39,7 @@ public:
 		cout << "~Furniture()\n";
 	}
 
-	void Add(int x, int y)
+	void Add(int x, int y) // метод для работы с недоступными полями private
 	{
 		length = length + x;
 		width = width + y;
@@ -49,7 +50,50 @@ public:
 
 };
 
-void Furniture::Reset()
+class Table: public Furniture
+{
+protected:
+	string color;
+
+public:
+
+	Table() : Furniture()
+	{
+		cout << "Table() : Furniture()\n";
+		color = "none";
+
+	}
+
+	Table(int l, int w, string clr) : Furniture(l,w)
+	{
+		cout << "Table(int l, int w, string clr) : Furniture(l,w))\n";
+		color = clr;
+		
+	}
+
+	Table(const Table& t) 
+	{
+		cout << "Table(const Table &f)\n";
+		length = t.length;
+		width = t.width;
+		color = t.color;
+	}
+
+	~Table()
+	{
+		cout << length << ", " << width << ", " << color << "\n";
+		cout << "~Table()\n";
+	}
+
+	void ChangeColor(string clr)
+	{
+		color = clr;
+		cout << "void ChangeColor(int clr) => color = " << color << "\n";
+	}
+
+};
+
+void Furniture::Reset() // реализация метода после определения
 {
 	length = 0;
 	width = 0;
@@ -66,12 +110,17 @@ int main()
 	Furniture* sub4 = new Furniture;
 	Furniture* sub5 = new Furniture(10, 15);
 	Furniture* sub6 = new Furniture(*sub5);
+	cout << "\n";
 
 	delete sub4;
 	delete sub5;
 	delete sub6;
+	cout << "\n";
 
 	sub2.Reset();
 	sub2.Add(5, 6);
+	cout << "\n";
 
+	Table tab1(1,2,"blue");
+	tab1.ChangeColor("red");
 }
